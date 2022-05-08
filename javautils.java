e167ba309ca7e505ea80af213a0dd3dd8adccc9a
Hemanthsivaram <sivaram40133@gmail.com>
2022-05-08 14:08:03 +0530

    2nd file uploading to remote repository

---
+++ javautils.java
@@ -0,0 +1,11 @@
+echo "Hellow world successfully pushed to remote repository"
+echo "so now i can understand one repository url con't be mapped with 2 alias names"
+echo "below is the error i got while mapping same url with diff alias names "
+<<err
+error: failed to push some refs to 'https://github.com/Hemanth-Devops-Testing/facebook.git'
+hint: Updates were rejected because the remote contains work that you do
+hint: not have locally. This is usually caused by another repository pushing
+hint: to the same ref. You may want to first integrate the remote changes
+hint: (e.g., 'git pull ...') before pushing again.
+hint: See the 'Note about fast-forwards' in 'git push --help' for details.
+err
